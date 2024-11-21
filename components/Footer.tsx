f162415159041ed27516b9a49f11c8f03b7319fb
T_Mori <morit0301@gmail.com>
2024-11-22 04:23:24 +0900

    COnstructions of Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full text-center bg-whitesmoke py-2 md:hidden">
+      <p>@2024 morimoli</p>
+    </div>
+  );
 };
 
 export default Footer;
