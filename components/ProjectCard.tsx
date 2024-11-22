a75e64b8abf8767e9f377eb50f9fcaa864b173ff
T_Mori <morit0301@gmail.com>
2024-11-22 18:45:52 +0900

    Constructions of SideMenu

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -25,14 +25,25 @@ const ProjectCard = ({
       transition={{ duration: 0.3 }}
       className="mb-4 p-4 border border-gray-200 rounded-lg bg-whitesmoke shadow-md"
     >
-      <img src={image} alt={`${name} Image`} />
-      <h3>{name}</h3>
-      <div>
-        <a href={githubUrl} target="_blank">
+      <img
+        src={image}
+        alt={`${name} Image`}
+        className="w-full h-auto mb-2 rounded-lg"
+      />
+      <h3 className="ml-2">{name}</h3>
+      <div className="flex gap-2 items-center w-full m-1">
+        <a
+          href={githubUrl}
+          target="_blank"
+          className="text-darkblue p-1 rounded-full text-lg"
+        >
           <AiFillGithub />
         </a>
         {deployed && (
-          <a href={projextUrl}>
+          <a
+            href={projextUrl}
+            className="text-darkblue p-1 rounded-full text-2xl"
+          >
             <AiFillEye />
           </a>
         )}
